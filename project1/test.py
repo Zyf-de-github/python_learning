c4109c740e491a8f1a724dec6d6ab453410ad46d
Zyf_de_github <3397337213@qq.com>
2025-06-21 04:09:29 +0800

    第一次提交

---
+++ project1/test.py
@@ -0,0 +1,25 @@
+# print("hello world")
+
+
+# str = "abcdefghijklmnopqrst"
+# print(str[0:5]) # prints "abcde"
+# print(str[5:10]) # prints "fghij"
+# print(str[10:-1])
+# print(str[0:])
+# print(str[10:])
+# print(str+"你好")
+# print(str*2)
+
+
+# input("\n\n按下 enter 键后退出。")
+# str = "abcdefghijklmnopqrst"
+# print(str[0:5]) # prints "abcde"
+
+
+# import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
+# 变量="变量"
+# print(变量)
+# 姓名 = "张三"  # 合法
+# π = 3.14159   # 合法
